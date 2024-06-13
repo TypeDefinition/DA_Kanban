f8f47a9181e5abeb8275197095d5c03b0935f524
TypeDefinition <lnxterry@gmail.com>
2024-06-13 10:19:55 +0800

    Did register and get all users

--- backend/database.js
+++ backend/database.js
@@ -2,12 +2,14 @@
 const mysql = require("mysql2")
 
 // Our database connection.
-const db = mysql.createPool({
-  host: `${process.env.DB_HOST}`,
-  user: `${process.env.DB_USERNAME}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`,
-  connectionLimit: 10,
-})
+const db = mysql
+  .createPool({
+    host: `${process.env.DB_HOST}`,
+    user: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    connectionLimit: 10,
+  })
+  .promise()
 
 module.exports = db
--- backend/routes.js
+++ backend/routes.js
@@ -5,17 +5,32 @@ const express = require("express")
 const router = express.Router()
 
 router.post("/register", (req, res) => {
-  const { username, password, email } = req.body
-  console.log(`${username}`)
-  console.log(`${password}`)
-  console.log(`${email}`)
+  async function doQuery(req, res) {
+    const { username, password, email } = req.body
 
-  const queryStatement = `INSERT INTO account (username, password, email, is_admin, is_enabled) VALUES ('${username}', '${password}', '${email}', false, true);`
-  const result = db.query(queryStatement)
-
-  // Find out via result if it succeeded or failed.
-  console.log(result)
-  res.status(200).send("Register")
+    // Because username, password and email are untrusted data from the end user,
+    // we cannot add it directly to the query in case of injection attacks.
+    // We instead use ?, and then pass in the values as an array at the end.
+    try {
+      const queryStatement = `INSERT INTO account (username, password, email, is_admin, is_enabled) VALUES (?, ?, ?, false, true);`
+      const result = await db.query(queryStatement, [username, password, email])
+      console.log(result)
+      res.status(200).send("Success")
+    } catch (e) {
+      console.log(e)
+      let msg = "Unknown error."
+      switch (e.code) {
+        case "ER_DUP_ENTRY":
+          msg = "This username or email has already been used."
+          break
+        case "ER_BAD_NULL_ERROR":
+          msg = "All required fields cannot be empty."
+          break
+      }
+      res.status(400).send(msg)
+    }
+  }
+  doQuery(req, res)
 })
 
 router.post("/login", (req, res) => {
@@ -23,12 +38,18 @@ router.post("/login", (req, res) => {
 })
 
 router.get("/users", (req, res) => {
-  // Do not select password.
-  const queryStatement = `SELECT (username, email, is_admin, is_enabled) from accounts;`
-  const result = db.query(queryStatement)
-  console.log(result)
-
-  res.status(200).send("Get All Users")
+  async function doQuery(req, res) {
+    try {
+      const queryStatement = `SELECT username, email, is_admin, is_enabled FROM account;`
+      const [rows] = await db.query(queryStatement)
+      console.log(rows)
+      res.status(200).send(rows)
+    } catch (e) {
+      console.log(e.sqlMessage)
+      res.status(400).send("Bad request.")
+    }
+  }
+  doQuery(req, res)
 })
 
 router.get("/user/:id", (req, res) => {
